1febb4a07f5598883310996daa02aaee1743516b
Sean Wieler <sbwieler@gmail.com>
2018-10-25 23:00:48 -0700

    remove sitebranding

--- header.php
+++ header.php
@@ -25,7 +25,7 @@
 	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'travelblog' ); ?></a>
 
 	<header id="masthead" class="site-header">
-		<div class="site-branding">
+		<!--<div class="site-branding">
 			<?php
 			//the_custom_logo();
 			if ( is_front_page() && is_home() ) :
@@ -42,7 +42,7 @@
 				?>
 				<p class="site-description"><?php echo $travelblog_description; /* WPCS: xss ok. */ ?></p>
 			<?php endif; ?>
-		</div><!-- .site-branding -->
+		</div> .site-branding -->
 
 		<nav id="site-navigation" class="main-navigation">
 			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'travelblog' ); ?></button>
