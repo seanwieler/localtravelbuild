957c3ffb8ae415377a00e1ddd207016d28895e8b
Sean Wieler <sbwieler@gmail.com>
2018-10-25 23:15:44 -0700

    adjust menu

--- front-page.php
+++ front-page.php
@@ -29,9 +29,7 @@ get_header();
 				<div><?php echo wp_get_attachment_image( $image, $size );?></div><?php
 			}?>
 
-			<?php if(get_field('travel_grid_shortcode')): ?>
-				<div class="black-bg"><span><?php echo do_shortcode('[travel_grid country="canada"]');?></span></div>
-			<?php endif;?>
+			<div class="black-bg"><span><?php echo do_shortcode('[travel_grid country="canada"]');?></span></div>
 
 			<?php if(get_field('section_1_content')): ?>
 				<div class="section-content-blackbg"> <?php echo get_post_meta(get_the_ID(), "section_1_content", true); ?> </div> 
